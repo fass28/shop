d5e4f3d6091384a01accee6bb9925798ce7d5a9f
fass28 <fass28@icloud.com>
2024-06-06 17:43:46 -0500

    adding api and grid

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,17 +1,25 @@
-const Card = () => {
-    return (
-        <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
-            <figure className='relative mb-2 w-full h-4/5'>
-                <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>Electronics</span>
-                <img className='w-full h-full object-cover rounded-lg' src="https://images.pexels.com/photos/8000624/pexels-photo-8000624.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"  alt='HeadsPhone' />
-                <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full  m-2 pb-1'><span>+</span></div>
-            </figure>
-            <p className='flex justify-between'>
-                <span className='text-sm font-light'>HeadPhone</span>
-                <span className='text-lg font-medium'>$300</span>
-            </p>
+const Card = ({data}) => {
+  return (
+    <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
+      <figure className="relative mb-2 w-full h-4/5">
+        <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
+          {data.category.name}
+        </span>
+        <img
+          className="w-full h-full object-cover rounded-lg"
+          src={data.images[0]}
+          alt="HeadsPhone"
+        />
+        <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full  m-2 p-1">
+          <span>+</span>
         </div>
-    )
-}
+      </figure>
+      <p className="flex justify-between">
+        <span className="text-sm font-light">{data.title}</span>
+        <span className="text-lg font-medium">${data.price}</span>
+      </p>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
